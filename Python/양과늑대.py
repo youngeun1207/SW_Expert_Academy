f45994492585a3189431ad8227d7dd73d4e62829
youngeun1207 <yesheen98@gmail.com>
2023-07-22 00:57:22 +0900

    프로그래머스 Lv.3 양과 늑대(2022 KAKAO BLIND RECRUITMENT 기출)

--- Python/양과늑대.py
+++ Python/양과늑대.py
@@ -5,7 +5,7 @@ def solution(info, edges):
     # 양이 늑대보다 많아야 자식 노드로 이동
     
     graph = dd(list)
-    visited = [False]*len(info) # 새로운 양 수집하면 visited 체크
+    visited = [False]*len(info)
     
     for edge in edges:
         graph[edge[0]].append(edge[1]) # 부모 노드 먼저 검증해서 자식 노드 검증 시간 줄이는 용
@@ -13,23 +13,23 @@ def solution(info, edges):
     answer = []
     visited[0] = True # root node: 0번 노드
     
-    def DFS(node, sheep, wolf):
+    def DFS(sheep, wolf):
         if sheep > wolf: # 갈 수 있어
             answer.append(sheep)
         else: # 잡아먹힌다!
             return 
         
         for p_node, c_nodes in graph.items():
-            if visited[p_node]: # 아까 도전했는데 안된 부분이면 재도전 하기 위해 부모 방문 여부 체크
+            if visited[p_node]: # 서브트리 순회 할 분기 포인트인지 확인
                 for c_node in c_nodes:
                     if not visited[c_node]:
                         visited[c_node] = True # 아래로 뻗어나갈 분기 포인트 체크
                         if info[c_node] == 0:
-                            DFS(c_node, sheep+1, wolf)
+                            DFS(sheep+1, wolf)
                         else:
-                            DFS(c_node, sheep, wolf+1)
+                            DFS(sheep, wolf+1)
                         visited[c_node] = False # 서브트리 다 순회했음
             
-    DFS(0, 1, 0)
+    DFS(1, 0)
     
     return max(answer)
